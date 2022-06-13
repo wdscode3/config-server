d7ff45943dd309600f672fe06ca071386fb42f71
wsilva.ekan <wsilva.ekan@iamspe.sp.gov.br>
2022-06-13 03:47:06 -0300

    Commit inicial

---
+++ src/main/java/br/gov/sp/iamspe/configserver/config/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package br.gov.sp.iamspe.configserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        .authorizeRequests()
+            .antMatchers("/encrypt", "/encrypt.html").hasRole("ADMIN")
+            .antMatchers("/decrypt", "/dencrypt.html").hasRole("ADMIN")
+            .anyRequest().authenticated()
+        .and()
+            .httpBasic()
+        .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("admin").password("{noop}" + env.getProperty("app.users.admin.password")).roles("ADMIN")
+            .and()
+            .withUser("user").password("{noop}" + env.getProperty("app.users.user.password")).roles("USER");
+    }
+    
+}
